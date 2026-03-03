2eeed2d0e764b547490d993ad0947c90ac3b4aac
Clement <clement.perrin9@etu.univ-lorraine.fr>
2026-03-03 14:48:32 +0100

    suppression vieux truc pas utile

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,31 @@
 import { app } from "./app";
+import { Country } from "./domain/entities/Country";
+import { City } from "./domain/entities/City";
+
+
 
 export default {
     port: process.env.PORT,
     fetch: app.fetch
 };
+
+function testCities() {
+    try {
+        const usa = new Country("USA", "us");
+
+        // Ville autorisée
+        const atlanta = new City(usa, "Atlanta");
+        console.log(`${atlanta.name} (${atlanta.country.name}) - ${atlanta.stadiumName} (${atlanta.stadiumCapacity})`);
+
+        // Une autre ville autorisée
+        const miami = new City(usa, "Miami");
+        console.log(`${miami.name} (${miami.country.name}) - ${miami.stadiumName} (${miami.stadiumCapacity})`);
+
+        // Ville non autorisée -> doit lancer une erreur
+        const paris = new City(usa, "Paris");
+    } catch (err: any) {
+        console.error("Erreur:", err.message);
+    }
+}
+
+testCities();
